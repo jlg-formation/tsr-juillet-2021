4c7022dad4cfdf128626bbc591a52f7d374f0149
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-01 17:28:57 +0200

    dev: added d3

--- front/src/Mendeleev.ts
+++ front/src/Mendeleev.ts
@@ -1,5 +1,13 @@
+import * as d3 from "d3";
+
 export class Mendeleev {
   constructor() {
     console.log("instantiate Mendeleev");
+    this.init();
+  }
+
+  async init() {
+    const csv = await d3.csv("./atomes.csv");
+    console.log("csv: ", csv);
   }
 }
