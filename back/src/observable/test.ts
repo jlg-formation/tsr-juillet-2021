85c9994b6d5de054264d2f40347739559caa9100
Jean-Louis GUÉNÉGO <jlguenego@gmail.com>
2021-07-02 17:24:04 +0200

    dev: observable

---
+++ back/src/observable/test.ts
@@ -0,0 +1,20 @@
+import {interval, of} from 'rxjs';
+import {map, startWith, take} from 'rxjs/operators';
+
+interval(1000)
+  .pipe(
+    map(n => 2 * n),
+    startWith('xxx'),
+    take(5)
+  )
+  .subscribe({
+    next: obj => {
+      console.log('obj: ', obj);
+    },
+    error: err => {
+      console.log('err: ', err);
+    },
+    complete: () => {
+      console.log('completed.');
+    },
+  });
